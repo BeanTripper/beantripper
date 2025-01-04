f6872ed5cd150b8aadb71860bdd3685ba72585fb
1125bid <dlfdlfdldh1125@naver.com>
2025-01-04 17:41:59 +0900

    add: login_logo 등록

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('LoginPage'),
+        child: Image.asset(
+          "assets/images/login_logo.png",
+          width: 210,
+        ),
       ),
     );
   }
