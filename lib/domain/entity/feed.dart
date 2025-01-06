17641cf43df1e910e892e9cfa8c2c212b3f52137
ParkChaeEun08 <pce980810@gmail.com>
2025-01-06 19:43:38 +0900

    add: imageurl, description

--- lib/domain/entity/feed.dart
+++ lib/domain/entity/feed.dart
@@ -1,4 +1,4 @@
-import 'package:bean_tripper/domain/entity/cafe.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Feed {
   int id;
@@ -7,6 +7,9 @@ class Feed {
   String cafeName;
   String writerId;
   String writerName;
+  String createdAt;
+  String imageUrl; // imageUrl 필드 추가
+  String description; // description 필드 추가
 
   Feed({
     required this.id,
@@ -15,5 +18,23 @@ class Feed {
     required this.cafeName,
     required this.writerId,
     required this.writerName,
+    required this.createdAt,
+    required this.imageUrl, // 필드 초기화 추가
+    required this.description, // 필드 초기화 추가
   });
+
+  factory Feed.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+    return Feed(
+      id: data['id'] ?? 0,
+      content: data['content'] ?? '',
+      cafeId: data['cafeId'] ?? '',
+      cafeName: data['cafeName'] ?? '',
+      writerId: data['writerId'] ?? '',
+      writerName: data['writerName'] ?? '',
+      createdAt: data['createdAt'] ?? '',
+      imageUrl: data['imageUrl'] ?? '', // 데이터 변환 로직 추가
+      description: data['description'] ?? '', // 데이터 변환 로직 추가
+    );
+  }
 }
