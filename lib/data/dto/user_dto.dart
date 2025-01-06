ecaf7fb697ab9ca7972064748ae6fc2978ece350
1125bid <dlfdlfdldh1125@naver.com>
2025-01-06 18:54:35 +0900

    fix: UserDto profile 맴버변수 생성

--- lib/data/dto/user_dto.dart
+++ lib/data/dto/user_dto.dart
@@ -1,19 +1,40 @@
+import 'dart:convert';
+
 class UserDto {
-  String id;
-  String name;
+  final String id;
+  final String name;
+  final String profile;
 
   UserDto({
     required this.id,
     required this.name,
+    required this.profile,
   });
 
-  factory UserDto.fromJson(Map<String, dynamic> map) =>
-      UserDto(id: map['id'], name: map['name']);
+  UserDto copyWith({
+    String? id,
+    String? name,
+    String? profile,
+  }) =>
+      UserDto(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        profile: profile ?? this.profile,
+      );
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-    };
-  }
+  factory UserDto.fromRawJson(String str) => UserDto.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory UserDto.fromJson(Map<String, dynamic> json) => UserDto(
+        id: json["id"],
+        name: json["name"],
+        profile: json["profile"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "profile": profile,
+      };
 }
