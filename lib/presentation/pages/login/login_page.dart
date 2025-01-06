ef870aa280b85c131f109627e5ce6ab74f0a4031
1125bid <dlfdlfdldh1125@naver.com>
2025-01-06 19:42:10 +0900

    fix: import 정리및 자잘한 정리

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -1,15 +1,12 @@
+import 'package:bean_tripper/presentation/pages/login/login_page_view_model.dart';
 import 'package:bean_tripper/presentation/pages/login/widget/custom_social_button.dart';
 import 'package:bean_tripper/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,32 +34,47 @@ class _LoginPageState extends State<LoginPage> {
                 ],
               ),
             ),
-            //TODO Consumer Widget 감싸기
-            Column(
-              children: [
-                Spacer(),
-                CustomSocialButton(
-                  text: "카카오로 시작하기",
-                  backgroundColor: const Color(0xFFFAE100),
-                  textColor: CustomColors.black,
-                  iconPath: "assets/images/kakao_icon.png",
-                  onPressed: () async {},
-                  iconSize: 20,
-                ),
-                const SizedBox(height: 20),
-                CustomSocialButton(
-                  text: "구글로 시작하기",
-                  backgroundColor: Colors.white,
-                  textColor: CustomColors.black,
-                  iconPath: "assets/images/google_icon.png",
-                  onPressed: () async {
-                    // TODO signInWithGoogle();
-                  },
-                  iconSize: 20,
-                ),
-                const SizedBox(height: 20),
-              ],
-            )
+            Consumer(builder: (context, ref, child) {
+              // final userState=ref.watch( loginPageViewModel );
+              // final userViewModel=ref.read(loginPageViewModel.notifier
+              // )
+//TODO 뷰모델 갖고오기.
+
+              return Column(
+                children: [
+                  Spacer(),
+                  CustomSocialButton(
+                    text: "카카오로 시작하기",
+                    backgroundColor: const Color(0xFFFAE100),
+                    textColor: CustomColors.black,
+                    iconPath: "assets/images/kakao_icon.png",
+                    onPressed: () async {},
+                    iconSize: 20,
+                  ),
+                  const SizedBox(height: 20),
+                  CustomSocialButton(
+                    text: "구글로 시작하기",
+                    backgroundColor: Colors.white,
+                    textColor: CustomColors.black,
+                    iconPath: "assets/images/google_icon.png",
+                    onPressed: () async {
+                      // TODO signInWithGoogle();
+                    },
+                    iconSize: 20,
+                  ),
+                  const SizedBox(height: 20),
+                  Divider(
+                    color: CustomColors.lightGrey,
+                    thickness: 1,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    '둘러보기',
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              );
+            })
           ],
         ),
       ),
