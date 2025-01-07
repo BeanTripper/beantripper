0420b71718e9dcd76042f5dcbef326d535a6241d
1125bid <dlfdlfdldh1125@naver.com>
2025-01-07 14:37:37 +0900

    fix: AppUser entity 매서드 추가 copyWith

--- lib/domain/entity/app_user.dart
+++ lib/domain/entity/app_user.dart
@@ -1,11 +1,40 @@
+import 'dart:convert';
+
 class AppUser {
-  String id;
-  String name;
-  String profile;
+  final String id;
+  final String name;
+  final String profile;
 
   AppUser({
     required this.id,
     required this.name,
     required this.profile,
   });
+
+  AppUser copyWith({
+    String? id,
+    String? name,
+    String? profile,
+  }) =>
+      AppUser(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        profile: profile ?? this.profile,
+      );
+
+  factory AppUser.fromRawJson(String str) => AppUser.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory AppUser.fromJson(Map<String, dynamic> json) => AppUser(
+        id: json["id"],
+        name: json["name"],
+        profile: json["profile"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "profile": profile,
+      };
 }
