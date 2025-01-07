3076245ef03a46d5a54a0fee7c7f41c575363e21
JinseongMOK <mjs4027@naver.com>
2025-01-07 09:49:23 +0900

    refactor: theme 데이터 파일 위치 변경

---
+++ lib/constant/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF222222);
+  static const Color darkGray = Color(0xFFAAAAAA);
+  static const Color lightGray = Color(0xFFD9D9D9);
+  static const Color brown = Color(0xFFA47764);
+  static const Color onError = Color(0xFFFD7563);
+  static const Color blue = Color(0xFF3A78F3);
+}
