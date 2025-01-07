d0230ccd8e17dc0ec017aac76c6882f55ddb3fc8
1125bid <dlfdlfdldh1125@naver.com>
2025-01-07 21:35:07 +0900

    merge issue

--- lib/presentation/pages/map/map_page.dart
+++ lib/presentation/pages/map/map_page.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
 import 'package:bean_tripper/constant/theme.dart';
+import 'package:bean_tripper/presentation/pages/map/map_view_model.dart';
+import 'package:bean_tripper/presentation/pages/map/widgets/map_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
